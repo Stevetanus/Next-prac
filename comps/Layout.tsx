de110c8058f8aa422a866bc4d0fbd5a011fee35c
Steventhibe <steventhibe@gmail.com>
2023-04-15 19:52:21 +0800

    intro, pages, other comps, links, layout

---
+++ comps/Layout.tsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="content">
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/about.tsx
@@ -0,0 +1,25 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non modi illo
+        exercitationem doloremque aut pariatur ut quo tenetur harum aliquam,
+        dolorum maxime neque ratione quos nostrum voluptatum dolore quas porro
+        eligendi, cum error. Repudiandae animi in exercitationem dolore beatae
+        atque debitis voluptatem! Doloribus, a. Eligendi doloremque inventore
+        molestiae iure. Nesciunt!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non modi illo
+        exercitationem doloremque aut pariatur ut quo tenetur harum aliquam,
+        dolorum maxime neque ratione quos nostrum voluptatum dolore quas porro
+        eligendi, cum error. Repudiandae animi in exercitationem dolore beatae
+        atque debitis voluptatem! Doloribus, a. Eligendi doloremque inventore
+        molestiae iure. Nesciunt!
+      </p>
+    </div>
+  );
+};
+
+export default About;
